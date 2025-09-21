c349ab89c25eba5d24934d37ecb3853e0a75408b
JodhvirTiwana1999 <jodhtiwana1999@gmail.com>
2025-09-21 17:57:07 +0530

    change router

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,62 @@
+// import React from 'react'
+// import Home from './Page/Home'
+// import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+// import About from './Page/About'
+// import Service from './Page/Service'
+// import Insights from './Page/ServiceInsights'
+// import Contact from './Page/Contact'
+// import Portfolio from './Page/Portfolio'
+// import Pricing from './Page/Pricing'
+// import Applayout from './Page/Applayout'
+
+// export default function App() {
+//   let Allrouter = createBrowserRouter([
+//      {
+//         path:'/',
+//         element:<Applayout/>,
+//         children:[
+//                     {
+//                          path:'/',
+//                          element:<Home/>,
+//                     },
+//                     {
+//                          path:'/About',
+//                          element:<About/>,
+//                     },
+//                     {
+//                          path:'/Service',
+//                          element:<Service/>,
+//                     },
+//                     {
+//                          path:'/Insights',
+//                          element:<Insights/>,
+//                     },
+//                     {
+//                          path:'/Contact',
+//                          element:<Contact/>,
+//                     },
+//                     {
+//                          path:'/Portfolio',
+//                          element:<Portfolio/>,
+//                     },
+//                     {
+//                          path:'/Pricing',
+//                          element:<Pricing/>,
+//                     }
+//         ]  
+//      }
+     
+     
+//   ])
+    
+//      return <RouterProvider router={Allrouter}/>
+// }
+
+//hashing router 
+
 import React from 'react'
 import Home from './Page/Home'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createHashRouter, RouterProvider } from 'react-router-dom' // yaha BrowserRouter hatao
 import About from './Page/About'
 import Service from './Page/Service'
 import Insights from './Page/ServiceInsights'
@@ -10,47 +66,23 @@ import Pricing from './Page/Pricing'
 import Applayout from './Page/Applayout'
 
 export default function App() {
-  let Allrouter = createBrowserRouter([
+  let Allrouter = createHashRouter([   // 👈 yaha bhi HashRouter
      {
-        path:'/',
-        element:<Applayout/>,
-        children:[
-                    {
-                         path:'/',
-                         element:<Home/>,
-                    },
-                    {
-                         path:'/About',
-                         element:<About/>,
-                    },
-                    {
-                         path:'/Service',
-                         element:<Service/>,
-                    },
-                    {
-                         path:'/Insights',
-                         element:<Insights/>,
-                    },
-                    {
-                         path:'/Contact',
-                         element:<Contact/>,
-                    },
-                    {
-                         path:'/Portfolio',
-                         element:<Portfolio/>,
-                    },
-                    {
-                         path:'/Pricing',
-                         element:<Pricing/>,
-                    }
+        path: '/',
+        element: <Applayout />,
+        children: [
+          { path: '/', element: <Home /> },
+          { path: '/About', element: <About /> },
+          { path: '/Service', element: <Service /> },
+          { path: '/Insights', element: <Insights /> },
+          { path: '/Contact', element: <Contact /> },
+          { path: '/Portfolio', element: <Portfolio /> },
+          { path: '/Pricing', element: <Pricing /> },
         ]  
      }
-     
-     
   ])
     
-     return <RouterProvider router={Allrouter}/>
+  return <RouterProvider router={Allrouter}/>
 }
-
 
 
