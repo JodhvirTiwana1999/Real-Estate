1b3c019648285436b0a04228cec970d1770d5a58
JodhvirTiwana1999 <jodhtiwana1999@gmail.com>
2025-09-22 12:00:43 +0530

    path

--- src/Page/Service.jsx
+++ src/Page/Service.jsx
@@ -16,7 +16,7 @@ export default function Service() {
   
      return (
       <>
-          <section data-aos="fade-down" className='bg-[url("./image/background-about-page.svg")] h-[300px] flex items-center justify-center text-white px-2' >
+          <section data-aos="fade-down" className='bg-[url("image/background-about-page.svg")] h-[300px] flex items-center justify-center text-white px-2' >
                <div className='text-center'>
                    <h4 className='xs:text-[30px] xm:text-[38px] '>Our Service Solution</h4>
                    <p className='xs:mt-[25px] xm:mt-[30px] font-[18px]'>We design, develop, and strategize to deliver valuable products.</p>
@@ -24,11 +24,11 @@ export default function Service() {
        </section>
        {/* this is top section for service */}
          <div className='pt-6 xs:block sm:hidden text-center'>
-                <h4 className='xs:block sm:hidden xs-text-[23px] text-[16px] sm:mt-[20px]  lg:mt-[42px] min-w-[180px]'><span><img src="/Real-Estate/public/image/line-about.svg" className='inline mr-3' alt="line"/></span>Services<span><img src="/Real-Estate/public/image/line-about.svg" className='inline ml-3' alt="line"/></span></h4>
+                <h4 className='xs:block sm:hidden xs-text-[23px] text-[16px] sm:mt-[20px]  lg:mt-[42px] min-w-[180px]'><span><img src="image/Line-about.svg" className='inline mr-3' alt="line"/></span>Services<span><img src="image/Line-about.svg" className='inline ml-3' alt="line"/></span></h4>
          </div>
        <section data-aos="fade-left" className='xs:h-auto xs:py-4 lg:py-8 lg:h-[572px] bg-[#FFFAFA] flex text-black sm:gap-[20px] md:gap-[50px] lg:gap-[90px] px-3 xs:flex-col sm:flex-row-reverse '>
              <div className='xs:w-[100%] sm:w-[50%] flex items-center justify-center'>
-                    <img src="/Real-Estate/public/image/Service/background-1.svg" className='sm:min-w-[260px] md:min-w-[350px]' alt="about"/>
+                    <img src="image/Service/background-1.svg" className='sm:min-w-[260px] md:min-w-[350px]' alt="about"/>
                </div>
                <div data-aos="fade-right" className='xs:w-[100%] sm:w-[50%] '>
                       <h4 className='xs:hidden sm:block text-[16px] sm:mt-[20px] lg:mt-[42px] min-w-[180px] sm:mb-[8px]'><span><img src="/Real-Estate/public/image/line-about.svg" className='inline mr-3' alt="line"/></span>Services<span><img src="/Real-Estate/public/image/line-about.svg" className='inline ml-3' alt="line"/></span></h4>
@@ -48,7 +48,7 @@ export default function Service() {
        </section>
         <section className='xs:h-auto xs:py-4 lg:py-8  bg-[#ffffff] flex text-black sm:gap-[20px] md:gap-[50px] lg:gap-[67px] px-3 xs:flex-col sm:flex-row '>
              <div data-aos="fade-left" className='xs:w-[100%] sm:w-[50%] flex items-center justify-center'>
-                    <img src="/Real-Estate/public/image/Service/background-2.svg" className='sm:min-w-[260px] md:min-w-[350px]' alt="about"/>
+                    <img src="image/Service/background-2.svg" className='sm:min-w-[260px] md:min-w-[350px]' alt="about"/>
                </div>
                <div data-aos="fade-right" className='xs:w-[100%] sm:w-[50%] flex flex-col justify-center'>
                      
@@ -65,7 +65,7 @@ export default function Service() {
        </section>
         <section className='xs:h-auto xs:py-4 lg:py-8 bg-[#FFFAFA] flex text-black sm:gap-[20px] md:gap-[50px] lg:gap-[90px] px-3 xs:flex-col sm:flex-row-reverse '>
              <div data-aos="fade-left" className='xs:w-[100%] sm:w-[50%] flex items-center justify-center'>
-                    <img src="/Real-Estate/public/image/Service/background-3.svg" className='sm:min-w-[260px] md:min-w-[350px]' alt="about"/>
+                    <img src="image/Service/background-3.svg" className='sm:min-w-[260px] md:min-w-[350px]' alt="about"/>
                </div>
                <div data-aos="fade-right" className='xs:w-[100%] sm:w-[50%] flex flex-col justify-center'>
                      
@@ -74,10 +74,10 @@ export default function Service() {
                         With our expert architecture services, we provide innovative designs tailored to your unique needs. Our experienced architects guide you through every stage, from conceptualization to completion, ensuring your vision comes to life with precision, functionality, and aesthetic appeal.
                       </p>
                     <ul className='xs:mt-4 xs:grid xs:grid-cols-1 xm:grid-cols-2 xs:gap-[12px] sm:block'>
-                         <li className='xs:text-[14px] md:text-[15px] lg:text-[18px]  sm:mt-[20px] md:mt-[20px] lg:mt-[35px]  truncate w-58'><span ><img src="/Real-Estate/public/image/check-box-about.svg" className='inline mr-2' alt="check" /></span>Customized designs for unique client needs</li>
-                         <li className='xs:text-[14px] md:text-[15px] lg:text-[18px]  sm:mt-[15px] md:mt-[15px] lg:mt-[25px] truncate w-58'><span ><img src="/Real-Estate/public/image/check-box-about.svg" className='inline mr-2' alt="check" /></span>Innovative solutions blending functionality and style</li>
-                         <li className='xs:text-[14px] md:text-[15px] lg:text-[18px]  sm:mt-[15px] md:mt-[15px] lg:mt-[25px] truncate w-58'><span ><img src="/Real-Estate/public/image/check-box-about.svg" className='inline mr-2' alt="check" /></span>Detailed planning from concept to completion</li>
-                         <li className='xs:text-[14px] md:text-[15px] lg:text-[18px]  sm:mt-[15px] md:mt-[15px] lg:mt-[25px] truncate w-58'><span ><img src="/Real-Estate/public/image/check-box-about.svg" className='inline mr-2' alt="check" /></span>Expert guidance through every architectural phase</li>
+                         <li className='xs:text-[14px] md:text-[15px] lg:text-[18px]  sm:mt-[20px] md:mt-[20px] lg:mt-[35px]  truncate w-58'><span ><img src="image/check-box-about.svg" className='inline mr-2' alt="check" /></span>Customized designs for unique client needs</li>
+                         <li className='xs:text-[14px] md:text-[15px] lg:text-[18px]  sm:mt-[15px] md:mt-[15px] lg:mt-[25px] truncate w-58'><span ><img src="image/check-box-about.svg" className='inline mr-2' alt="check" /></span>Innovative solutions blending functionality and style</li>
+                         <li className='xs:text-[14px] md:text-[15px] lg:text-[18px]  sm:mt-[15px] md:mt-[15px] lg:mt-[25px] truncate w-58'><span ><img src="image/check-box-about.svg" className='inline mr-2' alt="check" /></span>Detailed planning from concept to completion</li>
+                         <li className='xs:text-[14px] md:text-[15px] lg:text-[18px]  sm:mt-[15px] md:mt-[15px] lg:mt-[25px] truncate w-58'><span ><img src="image/check-box-about.svg" className='inline mr-2' alt="check" /></span>Expert guidance through every architectural phase</li>
                          
                     </ul>
 
@@ -85,7 +85,7 @@ export default function Service() {
        </section>
         <section className='xs:h-auto xs:py-4 lg:py-8  bg-[#FFFAFA] flex text-black sm:gap-[20px] md:gap-[50px] lg:gap-[90px] px-3 xs:flex-col sm:flex-row '>
              <div data-aos="fade-left" className='xs:w-[100%] sm:w-[50%] flex items-center justify-center'>
-                    <img src="/Real-Estate/public/image/Service/background-4.svg" className='sm:min-w-[260px] md:min-w-[350px]' alt="about"/>
+                    <img src="image/Service/background-4.svg" className='sm:min-w-[260px] md:min-w-[350px]' alt="about"/>
                </div>
                <div data-aos="fade-right" className='xs:w-[100%] sm:w-[50%] flex flex-col justify-center'>
                      
@@ -94,10 +94,10 @@ export default function Service() {
                         Land Acquisition Assistance helps you navigate the complex process of purchasing land. From identifying the right property to handling legal documentation and negotiations, we provide expert guidance, ensuring a smooth and hassle-free experience in acquiring the perfect land for your needs
                       </p>
                     <ul className='xs:mt-4 xs:grid xs:grid-cols-1 xm:grid-cols-2 xs:gap-[12px] sm:block'>
-                         <li className='xs:text-[14px] md:text-[15px] lg:text-[18px]  sm:mt-[20px] md:mt-[20px] lg:mt-[35px] truncate w-58'><span ><img src="/Real-Estate/public/image/check-box-about.svg" className='inline mr-2' alt="check" /></span>Expert guidance in identifying suitable land.</li>
-                         <li className='xs:text-[14px] md:text-[15px] lg:text-[18px]  sm:mt-[15px] md:mt-[15px] lg:mt-[25px] truncate w-58'><span ><img src="/Real-Estate/public/image/check-box-about.svg" className='inline mr-2' alt="check" /></span>Assistance with legal documentation and paperwork</li>
-                         <li className='xs:text-[14px] md:text-[15px] lg:text-[18px]  sm:mt-[15px] md:mt-[15px] lg:mt-[25px] truncate w-58'><span ><img src="/Real-Estate/public/image/check-box-about.svg" className='inline mr-2' alt="check" /></span>Assistance with legal documentation and paperwork</li>
-                         <li className='xs:text-[14px] md:text-[15px] lg:text-[18px]  sm:mt-[15px] md:mt-[15px] lg:mt-[25px] truncate w-58'><span ><img src="/Real-Estate/public/image/check-box-about.svg" className='inline mr-2' alt="check" /></span>Help with due diligence and land verification</li>
+                         <li className='xs:text-[14px] md:text-[15px] lg:text-[18px]  sm:mt-[20px] md:mt-[20px] lg:mt-[35px] truncate w-58'><span ><img src="image/check-box-about.svg" className='inline mr-2' alt="check" /></span>Expert guidance in identifying suitable land.</li>
+                         <li className='xs:text-[14px] md:text-[15px] lg:text-[18px]  sm:mt-[15px] md:mt-[15px] lg:mt-[25px] truncate w-58'><span ><img src="image/check-box-about.svg" className='inline mr-2' alt="check" /></span>Assistance with legal documentation and paperwork</li>
+                         <li className='xs:text-[14px] md:text-[15px] lg:text-[18px]  sm:mt-[15px] md:mt-[15px] lg:mt-[25px] truncate w-58'><span ><img src="image/check-box-about.svg" className='inline mr-2' alt="check" /></span>Assistance with legal documentation and paperwork</li>
+                         <li className='xs:text-[14px] md:text-[15px] lg:text-[18px]  sm:mt-[15px] md:mt-[15px] lg:mt-[25px] truncate w-58'><span ><img src="image/check-box-about.svg" className='inline mr-2' alt="check" /></span>Help with due diligence and land verification</li>
                          
                     </ul>
 
@@ -105,7 +105,7 @@ export default function Service() {
        </section>
        <section className='xs:h-auto xs:py-4 lg:py-8  bg-[#ffffff] flex text-black md:gap-[50px] lg:gap-[90px] px-3  xs:flex-col sm:flex-row-reverse'>
              <div data-aos="fade-left" className='xs:w-[100%] sm:w-[50%] flex items-center justify-center'>
-                    <img src="/Real-Estate/public/image/Service/background-5.svg" className='sm:min-w-[260px] md:min-w-[350px]' alt="about"/>
+                    <img src="image/Service/background-5.svg" className='sm:min-w-[260px] md:min-w-[350px]' alt="about"/>
                </div>
                <div data-aos="fade-right" className='xs:w-[100%] sm:w-[50%] flex flex-col justify-center'>
                      
@@ -114,10 +114,10 @@ export default function Service() {
                        Sustainable Smart Homes combine advanced technology with eco-friendly design to create energy-efficient living spaces. These homes use smart systems to optimize energy use, reducing waste and lowering utility costs.  With integrated solar panels, efficient appliances, and intelligent heating/cooling systems, Sustainable Smart Homes minimize environmental impact while providing comfort and convenience
                       </p>
                     <ul className='xs:mt-4 xs:grid xs:grid-cols-1 xm:grid-cols-2 xs:gap-[12px] sm:block'>
-                         <li className='xs:text-[14px] md:text-[15px] lg:text-[18px]  sm:mt-[20px] md:mt-[20px] lg:mt-[35px] truncate w-58'><span ><img src="/Real-Estate/public/image/check-box-about.svg" className='inline mr-2' alt="check" /></span>Metaverse Features in Home.</li>
-                         <li className='xs:text-[14px] md:text-[15px] lg:text-[18px]  sm:mt-[15px] md:mt-[15px] lg:mt-[25px] truncate w-58'><span ><img src="/Real-Estate/public/image/check-box-about.svg" className='inline mr-2' alt="check" /></span>Energy-saving lighting and appliances</li>
-                         <li className='xs:text-[14px] md:text-[15px] lg:text-[18px]  sm:mt-[15px] md:mt-[15px] lg:mt-[25px] truncate w-58'><span ><img src="/Real-Estate/public/image/check-box-about.svg" className='inline mr-2' alt="check" /></span>Smartphone-controlled temperature management.</li>
-                         <li className='xs:text-[14px] md:text-[15px] lg:text-[18px]  sm:mt-[15px] md:mt-[15px] lg:mt-[25px] truncate w-58'><span ><img src="/Real-Estate/public/image/check-box-about.svg" className='inline mr-2' alt="check" /></span>Eco-Friendly Materials</li>
+                         <li className='xs:text-[14px] md:text-[15px] lg:text-[18px]  sm:mt-[20px] md:mt-[20px] lg:mt-[35px] truncate w-58'><span ><img src="image/check-box-about.svg" className='inline mr-2' alt="check" /></span>Metaverse Features in Home.</li>
+                         <li className='xs:text-[14px] md:text-[15px] lg:text-[18px]  sm:mt-[15px] md:mt-[15px] lg:mt-[25px] truncate w-58'><span ><img src="image/check-box-about.svg" className='inline mr-2' alt="check" /></span>Energy-saving lighting and appliances</li>
+                         <li className='xs:text-[14px] md:text-[15px] lg:text-[18px]  sm:mt-[15px] md:mt-[15px] lg:mt-[25px] truncate w-58'><span ><img src="image/check-box-about.svg" className='inline mr-2' alt="check" /></span>Smartphone-controlled temperature management.</li>
+                         <li className='xs:text-[14px] md:text-[15px] lg:text-[18px]  sm:mt-[15px] md:mt-[15px] lg:mt-[25px] truncate w-58'><span ><img src="image/check-box-about.svg" className='inline mr-2' alt="check" /></span>Eco-Friendly Materials</li>
                          
                     </ul>
 
